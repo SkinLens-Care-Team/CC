0c8eae7b6da31c383eeb3927ec35f0e1358a6e2a
Lihardo Purba <lihardompurba@gmail.com>
2024-11-25 19:32:00 +0700

    Create RestAPI.py
    adding restAPI code deployment for model

---
+++ RestAPI.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+import requests  
+app = Flask(__name__)
+
+MODEL_API_URL = "http://[YOUR_COMPUTE_ENGINE_IP]:8080/predict"
+
+@app.route('/get-prediction', methods=['POST'])
+def get_prediction():
+    image = request.files['image']
+    
+    response = requests.post(MODEL_API_URL, files={'image': image})
+    
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
